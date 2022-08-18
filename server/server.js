efeba47de0f476e437db1e45489f0ad2405bbca0
julie <j.u.veiga@hotmail.com>
2022-08-18 16:58:08 +0200

    #4 Refresh Token Automatically updated

--- server/server.js
+++ server/server.js
@@ -1,32 +1,55 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const spotifyWebApi = require('spotify-web-api-node');
-
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/login', (req, res) => {
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientID: "65907f8f732941c4b784206e99c3215e",
+    clientSecret: "93905a15f2f44552862e8d57dcf294b3",
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.post("/login", (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: 'http://localhost:3000',
-    clientID: '65907f8f732941c4b784206e99c3215e', 
-    clientSecret: '93905a15f2f44552862e8d57dcf294b3' 
-  })
+    redirectUri: "http://localhost:3000",
+    clientID: "65907f8f732941c4b784206e99c3215e",
+    clientSecret: "93905a15f2f44552862e8d57dcf294b3",
+  });
 
-  spotifyApi.authorizationCodeGrant(code).then(data => {
-    res.json({
-      accessToken: data.body.access_token,
-      refreshToken: data.body.refresh_token,
-      expiresIn: data.body.expires_in
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
     })
-  })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
 
-  .catch(() => {
-    res.sendStatus(400)
-  })
-})
-
-app.listen(3001)
+app.listen(3001);
