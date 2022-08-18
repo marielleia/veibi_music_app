7def6cac21b4a9affe5bfa77ded7d83532435f9d
Marielle <marielle.ibias@example.com>
2022-08-18 15:03:32 +0200

    #3 useAuth Hook ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./Login";
+import Dashboard from "./Dashboard";
 
+const code = new URLSearchParams(window.location.search).get('code')
 function App() {
-  return <Login />;
+  return code ? <Dashboard code={code} />:<Login />;
 }
 
 export default App;
---
+++ client/src/useAuth.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useAuth(code) {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/login", {
+        code,
+      })
+      .then((res) => {
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, null, "/");
+      })
+      .catch(() => {
+        window.location = "/";
+      });
+  }, [code]);
+
+  return accessToken;
+}
