a42ca93926ddf5c2fc39f554869676bd6a3e8b02
Marielle <marielle.ibias@example.com>
2022-08-19 15:07:55 +0200

    #4 refresh token automatically updated

--- client/src/Login.js
+++ client/src/Login.js
@@ -7,7 +7,7 @@ const AUTH_URL =
 export default function Login() {
   return (
     <Container
-      classname="d-flex justify-content-center align-items-center"
+      className="d-flex justify-content-center align-items-center"
       style={{ minHeight: "100vh"}}
     >
       <a className="btn btn-success btn-lg" href={AUTH_URL}>
--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -6,25 +6,25 @@ export default function useAuth(code) {
   const [refreshToken, setRefreshToken] = useState();
   const [expiresIn, setExpiresIn] = useState();
 
-  console.log(refreshToken)
-
   useEffect(() => {
     axios
-      .post("http://localhost:3001/login", {
-        code,
-      })
-      .then((res) => {
-        setAccessToken(res.data.accessToken);
-        setRefreshToken(res.data.refreshToken);
-        setExpiresIn(res.data.expiresIn);
-        window.history.pushState({}, null, "/");
-      })
-      .catch(() => {
-        window.location = "/";
-      });
+    .post("http://localhost:3001/login", {
+      code,
+    })
+    .then((res) => {
+      setAccessToken(res.data.accessToken);
+      setRefreshToken(res.data.refreshToken);
+      setExpiresIn(res.data.expiresIn);
+      window.history.pushState({}, null, "/");
+    })
+    .catch(() => {
+      window.location = "/";
+    });
   }, [code]);
 
   useEffect(() => {
+  if (!refreshToken || !expiresIn) return
+  const interval = setInterval (()=>{
     axios
       .post("http://localhost:3001/refresh", {
         refreshToken,
@@ -35,7 +35,10 @@ export default function useAuth(code) {
       })
       .catch(() => {
         window.location = "/";
-      });
+      })
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearInterval(interval)
   }, [refreshToken, expiresIn])
 
   return accessToken;
