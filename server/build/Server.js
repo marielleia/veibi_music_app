a745feb7ac2e7545c2d23d45aaddb76ca058773d
Marielle <marielle.ibias@example.com>
2022-08-26 16:30:04 +0200

    package json update

--- server/build/Server.js
+++ server/build/Server.js
@@ -8,5 +8,5 @@ var App_1 = __importDefault(require("./App"));
 var server = http_1["default"].createServer(App_1["default"]);
 var PORT = process.env.PORT || 3001;
 server.listen(PORT, function () {
-    console.log("\uD83D\uDE80 Server ready at http://localhost:".concat(PORT));
+    console.log(" Server ready at http://localhost:".concat(PORT, "\uD83D\uDE80"));
 });
