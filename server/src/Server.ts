bd93a042f99ddf10c9e3f7224813a38d6215bc94
julie <j.u.veiga@hotmail.com>
2022-08-26 16:19:56 +0200

    updated package.json

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -6,6 +6,6 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, ()=> {
-  console.log(`🚀 Server ready at http://localhost:${PORT}`);
+  console.log(` Server ready at http://localhost:${PORT}🚀`);
 });
 
